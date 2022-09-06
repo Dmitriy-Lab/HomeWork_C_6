1d63d6fcac8991483991494dbc33968d7280b754
Dmitry-Lab <poznavshii@gmail.com>
2022-09-06 10:51:34 +0400

    Home1

---
+++ 1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);  // Задача свелась к написанию этой строки. Как говорится, гугл в помощь:)
+int sum = 0;
+
+for( int i = 0; i < array.Length ; i++)
+{
+    if ( array[i] > 0)
+      {
+        sum++;
+      }
+}
+Console.WriteLine("Чисел больше нуля: "+sum);
